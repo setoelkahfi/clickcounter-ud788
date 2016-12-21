48e80373ff7f8923a9faf398fa79942818e16a5c
Seto Elkahfi <setoelkahfi@gmail.com>
2016-12-21 20:15:24 +0700

    Add UILabel and UIButton

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -12,6 +12,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let label = UILabel()
+        label.frame = CGRect.init(x: 150, y: 150, width: 60, height: 60)
+        label.text = "0"
+        self.view.addSubview(label)
+        
+        let button = UIButton()
+        button.frame = CGRect.init(x: 150, y: 250, width: 60, height: 60)
+        button.setTitle("Click", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        self.view.addSubview(button)
         
     }
 
