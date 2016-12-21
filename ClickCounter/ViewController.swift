8f8efb83815992dff7f1c3516b11fc662b8ca49e
Seto Elkahfi <setoelkahfi@gmail.com>
2016-12-21 12:06:07 +0700

    Initial commit

---
+++ ClickCounter/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ClickCounter
+//
+//  Created by SDMobile on 12/21/16.
+//  Copyright © 2016 Seto Elkahfi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
