f364624e074ed3e2f9464146e68d5d108cf23233
Seto Elkahfi <setoelkahfi@gmail.com>
2016-12-23 16:53:51 +0700

    Connect the code to the story board.

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -11,34 +11,35 @@ import UIKit
 class ViewController: UIViewController {
     
     var count = 0
-    var label: UILabel!
+    
+    @IBOutlet var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let label = UILabel()
-        label.frame = CGRect.init(x: 150, y: 150, width: 60, height: 60)
-        label.text = "0"
-        self.view.addSubview(label)
-        self.label = label
-        
-        let button = UIButton()
-        button.frame = CGRect.init(x: 150, y: 250, width: 100, height: 60)
-        button.setTitle("Increment", for: .normal)
-        button.setTitleColor(UIColor.blue, for: .normal)
-        self.view.addSubview(button)
-     
-        button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControlEvents.touchUpInside)
-        
-        let buttonDecrement = UIButton()
-        buttonDecrement.frame = CGRect.init(x: 50, y: 250, width: 100, height: 60)
-        buttonDecrement.setTitleColor(UIColor.red, for: .normal)
-        buttonDecrement.setTitle("Decrement", for: .normal)
-        self.view.addSubview(buttonDecrement)
-        
-        buttonDecrement.addTarget(self, action: #selector(ViewController.decrementCount), for: UIControlEvents.touchUpInside)
+//        let label = UILabel()
+//        label.frame = CGRect.init(x: 150, y: 150, width: 60, height: 60)
+//        label.text = "0"
+//        self.view.addSubview(label)
+//        self.label = label
+//        
+//        let button = UIButton()
+//        button.frame = CGRect.init(x: 150, y: 250, width: 100, height: 60)
+//        button.setTitle("Increment", for: .normal)
+//        button.setTitleColor(UIColor.blue, for: .normal)
+//        self.view.addSubview(button)
+//     
+//        button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControlEvents.touchUpInside)
+//        
+//        let buttonDecrement = UIButton()
+//        buttonDecrement.frame = CGRect.init(x: 50, y: 250, width: 100, height: 60)
+//        buttonDecrement.setTitleColor(UIColor.red, for: .normal)
+//        buttonDecrement.setTitle("Decrement", for: .normal)
+//        self.view.addSubview(buttonDecrement)
+//        
+//        buttonDecrement.addTarget(self, action: #selector(ViewController.decrementCount), for: UIControlEvents.touchUpInside)
     }
     
-    func incrementCount() {
+    @IBAction func incrementCount() {
         self.count = self.count + 1;
         self.label.text = "\(self.count)"
     }
